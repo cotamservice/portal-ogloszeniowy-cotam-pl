4b36ec7efeb4be6c3fe0537e1bf4f38ffcca851d
AntonMiracle <b.anton.m.1986@gmail.com>
2020-07-29 13:28:18 +0200

    update registration

--- front-end-app/src/app/registration/registration.component.ts
+++ front-end-app/src/app/registration/registration.component.ts
@@ -6,6 +6,7 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
+
 export class RegistrationComponent implements OnInit {
   private elementPickedBackgroundColor = '#e9bc09';
   private categoryBackgroundColor = 'transparent';
@@ -17,59 +18,78 @@ export class RegistrationComponent implements OnInit {
   private registrationBrokerFormId = 'registration-broker-form';
   private lastOpenFormId = '';
 
+  registrationTitle = 'rejestracja';
+  loginTitle = 'login'
+
+  isLoginFormOpen: boolean = false;
+  loginElementTextValue = 'logowanie';
+
+  isRegistrationIndividualFormOpen: boolean = false;
+  registrationIndividualElementTextValue = 'indywidualne';
+
+  isRegistrationCommissionFormOpen: boolean = false;
+  registrationCommissionElementTextValue = 'komis';
+
+  isRegistrationBrokerFormOpen: boolean = false;
+  registrationBrokerElementTextValue = 'dealer/broker';
+
+
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit(): void {
-    if (this.router.url.endsWith("login")) {
-      this.pickLogin();
-    } else if (this.router.url.endsWith("registration")) {
-      this.pickIndywidualne();
-    }
+    // if (this.router.url.endsWith("login")) {
+    //   this.openLogin();
+    // } else if (this.router.url.endsWith("registration")) {
+    //   this.pickIndywidualne();
+    // }
   }
 
   pickElement(elementId: string): void {
-    if (this.lastPickedElementId !== '') {
-      document.getElementById(this.lastPickedElementId).style.backgroundColor = this.categoryBackgroundColor;
-    }
-    let pickedCategory = document.getElementById(elementId);
-    pickedCategory.style.backgroundColor = this.elementPickedBackgroundColor;
-    this.lastPickedElementId = elementId;
+    // if (this.lastPickedElementId !== '') {
+    //   document.getElementById(this.lastPickedElementId).style.backgroundColor = this.categoryBackgroundColor;
+    // }
+    // let pickedCategory = document.getElementById(elementId);
+    // pickedCategory.style.backgroundColor = this.elementPickedBackgroundColor;
+    // this.lastPickedElementId = elementId;
   }
 
   setRegistrationTitle(title: string): void {
-    document.getElementById('registration-title').textContent = title;
+    // document.getElementById('registration-title').textContent = title;
   }
 
   openCloseForm(formId: string): void {
-    if (this.lastOpenFormId !== '') {
-      document.getElementById(this.lastOpenFormId).style.display = 'none';
-    }
-    this.lastOpenFormId = formId;
-    document.getElementById(formId).style.display = 'block';
+    // if (this.lastOpenFormId !== '') {
+    //   document.getElementById(this.lastOpenFormId).style.display = 'none';
+    // }
+    // this.lastOpenFormId = formId;
+    // document.getElementById(formId).style.display = 'block';
   }
 
-  pickLogin(): void {
-    this.pickElement('login');
-    this.setRegistrationTitle('logowanie');
-    this.openCloseForm(this.loginFormId);
+  closeAllForms(): void {
+    this.isLoginFormOpen = false;
+    this.isRegistrationIndividualFormOpen = false;
+    this.isRegistrationCommissionFormOpen = false;
+    this.isRegistrationBrokerFormOpen = false;
   }
 
-  pickIndywidualne(): void {
-    this.pickElement('registration-indywidualne');
-    this.setRegistrationTitle('rejestracja');
-    this.openCloseForm(this.registrationIndywidualneFormId);
+  openLoginForm(): void {
+    this.closeAllForms();
+    this.isLoginFormOpen = true;
   }
 
-  pickKomis(): void {
-    this.pickElement('registration-komis');
-    this.setRegistrationTitle('rejestracja');
-    this.openCloseForm(this.registrationKomisFormId);
+  openRegistrationIndividualForm(): void {
+    this.closeAllForms();
+    this.isRegistrationIndividualFormOpen = true;
   }
 
-  pickBroker(): void {
-    this.pickElement('registration-broker');
-    this.setRegistrationTitle('rejestracja');
-    this.openCloseForm(this.registrationBrokerFormId);
+  openRegistrationCommissionForm(): void {
+    this.closeAllForms();
+    this.isRegistrationCommissionFormOpen = true;
+  }
+
+  openRegistrationBrokerForm(): void {
+    this.closeAllForms();
+    this.isRegistrationBrokerFormOpen = true;
   }
 }
