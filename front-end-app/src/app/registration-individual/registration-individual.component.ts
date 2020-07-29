00d5060ae91d1ddeecd7249a6f88beee0de8ca97
AntonMiracle <b.anton.m.1986@gmail.com>
2020-07-29 14:29:04 +0200

    add registration-individual

---
+++ front-end-app/src/app/registration-individual/registration-individual.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-registration-individual',
+  templateUrl: './registration-individual.component.html',
+  styleUrls: ['./registration-individual.component.css']
+})
+export class RegistrationIndividualComponent implements OnInit {
+  registrationButtonTitleValue = 'Zarejestruje się';
+
+  inputEmailId='id';
+  inputEmailLabelValue='Email';
+  inputEmailPlaceholderValue='wpiś email';
+
+  inputPasswordId='password';
+  inputPasswordLabelValue='Hasło';
+  inputPasswordPlaceholderValue='wpiś hasło';
+
+  inputPasswordConfirmId='confirm';
+  inputPasswordConfirmLabelValue='Powtóż hasło';
+  inputPasswordConfirmPlaceholderValue='powtóż hasło';
+
+  inputWordId='word';
+  inputWordLabelValue='Słowo kluczowe';
+  inputWordPlaceholderValue='wpiś słowo kluczowe';
+
+  inputAcceptCheckboxId = 'accept';
+  inputAcceptCheckboxTitleValue = 'Akceptuję regulamin serwisu';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
