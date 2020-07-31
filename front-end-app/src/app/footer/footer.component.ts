c7fbcbe71ef26aeae9f86befe759eca15e269c8b
AntonMiracle <b.anton.m.1986@gmail.com>
2020-07-31 12:38:12 +0200

    update footer

--- front-end-app/src/app/footer/footer.component.ts
+++ front-end-app/src/app/footer/footer.component.ts
@@ -19,6 +19,22 @@ export class FooterComponent implements OnInit {
 
   }
 
+  privacyPoliticLinkName: string = 'Polityka prywatności';
+  adLinkName: string = 'Reklama';
+  priceLinkName: string = 'Cennik';
+  helpLinkName: string = 'Pomoc';
+  contactLinkName: string = 'Kontakt';
+  regulationLinkName: string = 'regulaminu';
+  regulationAcceptationText: string = 'Korzystanie z serwisu oznacza akceptację';
+
+  isLogout(): boolean {
+    return true;
+  }
+
+  footerLogoutTitle:string = 'Zarejestruj się już teraz!';
+  footerLogoutText:string = 'Dzięki rejestracji będziesz mógł zarządzać dodanymi ogłoszeniami, a także dodawać\n' +
+    '          ogłoszenia do ulubionych, aby mieć do nich łatwy dostęp w każdej chwili!';
+
   redirectTo(pagename: string): void {
     this.router.navigate(['/' + pagename]);
   }
