f0321df6b987997a9cb8c03c570b6947a60f775c
AntonMiracle <b.anton.m.1986@gmail.com>
2020-08-04 10:12:33 +0200

    install angular2-flash-message

--- front-end-app/src/app/app.module.ts
+++ front-end-app/src/app/app.module.ts
@@ -1,41 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { FooterComponent } from './footer/footer.component';
-import { AuthenticateComponent } from './authenticate/authenticate.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {FooterComponent} from './footer/footer.component';
+import {AuthenticateComponent} from './authenticate/authenticate.component';
+import {HomeComponent} from './home/home.component';
 
-import { ToStartArrowComponent } from './to-start-arrow/to-start-arrow.component';
-import { RegistrationLoginComponent } from './registration-login/registration-login.component';
-import { RegistrationIndividualComponent } from './registration-individual/registration-individual.component';
-import { RegistrationCommissionComponent } from './registration-commission/registration-commission.component';
-import { RegistrationBrokerComponent } from './registration-broker/registration-broker.component';
+import {ToStartArrowComponent} from './to-start-arrow/to-start-arrow.component';
+import {RegistrationLoginComponent} from './registration-login/registration-login.component';
+import {RegistrationIndividualComponent} from './registration-individual/registration-individual.component';
+import {RegistrationCommissionComponent} from './registration-commission/registration-commission.component';
+import {RegistrationBrokerComponent} from './registration-broker/registration-broker.component';
 import {FormsModule} from "@angular/forms";
+import {FlashMessagesModule} from "angular2-flash-messages";
+import {RegistrationFormValidationService} from "./registration-form-validation.service";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        RegistrationComponent,
-        FooterComponent,
-        AuthenticateComponent,
-        HomeComponent,
-        ToStartArrowComponent,
-        RegistrationLoginComponent,
-        RegistrationIndividualComponent,
-        RegistrationCommissionComponent,
-        RegistrationBrokerComponent,
-    ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    FooterComponent,
+    AuthenticateComponent,
+    HomeComponent,
+    ToStartArrowComponent,
+    RegistrationLoginComponent,
+    RegistrationIndividualComponent,
+    RegistrationCommissionComponent,
+    RegistrationBrokerComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [RegistrationFormValidationService],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
