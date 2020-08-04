2e9f55729a1246d43b6e26009fa9aa7fb8fc5b07
AntonMiracle <b.anton.m.1986@gmail.com>
2020-08-04 20:12:57 +0200

    update routing

--- front-end-app/src/app/app-routing.module.ts
+++ front-end-app/src/app/app-routing.module.ts
@@ -1,22 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {RegistrationComponent} from "./registration/registration.component";
 import {AuthenticateComponent} from "./authenticate/authenticate.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'registration', component: RegistrationComponent,},
-  {path: 'registration/individual', component: RegistrationComponent,},
-  {path: 'registration/commission', component: RegistrationComponent,},
-  {path: 'registration/broker', component: RegistrationComponent,},
+  {
+    path: 'registration', component: RegistrationComponent,
+    children: [
+      {path: 'individual', component: RegistrationComponent},
+      {path: 'commission', component: RegistrationComponent},
+      {path: 'broker', component: RegistrationComponent},
+    ]
+  },
   {path: 'login', component: RegistrationComponent},
   {path: 'authentication', component: AuthenticateComponent},
   {path: '**', component: HomeComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
