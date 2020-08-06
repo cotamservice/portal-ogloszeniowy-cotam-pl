931fe968ae9a715e922cb449da1f6201de8beeea
AntonMiracle <b.anton.m.1986@gmail.com>
2020-08-06 13:33:28 +0200

    update registration component hide login and registration form if login

--- front-end-app/src/app/registration/registration.component.ts
+++ front-end-app/src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
+import {AuthenticateService} from "../service/authenticate/authenticate.service";
 
 @Component({
   selector: 'app-registration',
@@ -26,20 +27,25 @@ export class RegistrationComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private router: Router) {
+    private router: Router,
+    private authenticateS: AuthenticateService) {
   }
 
   ngOnInit(): void {
-    if (this.router.url.endsWith("login")) {
-      this.openLoginForm();
-    } else if (this.router.url.endsWith("registration")) {
-      this.closeAllForms();
-    } else if (this.router.url.endsWith("registration/individual")) {
-      this.openRegistrationIndividualForm();
-    } else if (this.router.url.endsWith("registration/commission")) {
-      this.openRegistrationCommissionForm();
-    } else if (this.router.url.endsWith("registration/broker")) {
-      this.openRegistrationBrokerForm();
+    if (this.authenticateS.isAuthenticate()) {
+      this.router.navigate(['dashboard']);
+    } else {
+      if (this.router.url.endsWith("login")) {
+        this.openLoginForm();
+      } else if (this.router.url.endsWith("registration")) {
+        this.closeAllForms();
+      } else if (this.router.url.endsWith("registration/individual")) {
+        this.openRegistrationIndividualForm();
+      } else if (this.router.url.endsWith("registration/commission")) {
+        this.openRegistrationCommissionForm();
+      } else if (this.router.url.endsWith("registration/broker")) {
+        this.openRegistrationBrokerForm();
+      }
     }
   }
 
