8617d84f97dcb8c8386e3b99f18caf4799c57c95
AntonMiracle <b.anton.m.1986@gmail.com>
2020-09-16 15:00:40 +0200

    update state to server side

--- front-end-app/src/app/service/state/state.service.ts
+++ front-end-app/src/app/service/state/state.service.ts
@@ -1,15 +1,22 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
+  private localhost: string = 'http://localhost:3000/';
 
-  constructor() {
+  constructor(private http: HttpClient,
+  ) {
   }
 
-  getStates(): Array<Array<string>> {
-    let result = [];
-    return result;
+  getAllStates() {
+    let headers = new HttpHeaders();
+    headers.append('Content-type', 'application/json');
+    return this.http
+      .get(this.localhost + 'state/all', {headers: headers})
+      .pipe(map(res => res));
   }
 }
