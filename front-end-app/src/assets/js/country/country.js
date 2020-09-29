201513526cef14851cfb0c366ff07445d41edb41
AntonMiracle <b.anton.m.1986@gmail.com>
2020-09-29 18:49:46 +0200

    update country.js fix browser locale

--- front-end-app/src/assets/js/country/country.js
+++ front-end-app/src/assets/js/country/country.js
@@ -2,9 +2,17 @@ export function getBrowserLocale() {
   let locales = navigator.languages === undefined
     ? [navigator.language]
     : navigator.languages;
+  console.log("LOCALES: " + locales)
+  console.log("LOCALES: " + locales[0])
+  console.log("LOCALES: " + locales[1])
+  console.log("LOCALES: " + locales[2])
+  console.log("LOCALES: " + locales[3])
+  console.log("LOCALES: " + locales[4])
   if (locales && locales[0].length > 4) {
     return locales[0].trim().split(/-|_/)[0];
+  } else if(locales && locales[0].length === 2){
+    return locales[0];
   } else {
-    return '';
+    return ''
   }
 }
