1431720ff2f49478ce9f0c2031aca57ae203962e
AntonMiracle <b.anton.m.1986@gmail.com>
2020-09-29 13:01:41 +0200

    add image-carousel.component

--- front-end-app/src/app/image-carousel/image-carousel.component.ts
+++ front-end-app/src/app/image-carousel/image-carousel.component.ts
@@ -19,34 +19,69 @@ export class ImageCarouselComponent implements OnInit {
   activeList: CarouselImg[];
   activeListFirstIndex: number;
   activeListLastIndex: number;
+  activeImgFS: CarouselImg;
+  activeListFS: CarouselImg[];
+  activeListFirstIndexFS: number;
+  activeListLastIndexFS: number;
+  isFullScreen: boolean;
 
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.activeListFirstIndex = 0;
-    this.activeListLastIndex = this.values.length < 6 ? this.values.length - 1 : 4;
+    this.activeListFirstIndexFS = this.activeListFirstIndex = 0;
+    this.activeListLastIndexFS = this.activeListLastIndex = this.values.length < 6 ? this.values.length - 1 : 4;
     this.setActiveList();
     this.setActive(undefined);
   }
 
   private setActiveList() {
-    this.activeList = [];
+    if (this.isFullScreen) {
+      this.activeListFS = [];
+    } else {
+      this.activeList = [];
+    }
+
     let index = 0;
     let activeListIndex = 0;
     for (let value of this.values) {
-      if (index >= this.activeListFirstIndex && index <= this.activeListLastIndex) {
+      if (index >= this.getActiveFirstIndex() && index <= this.getActiveLastIndex()) {
         let img = new CarouselImg();
         img.value = value;
         img.index = index;
         img.activeListIndex = activeListIndex;
-        this.activeList.push(img);
+        this.getActiveList().push(img);
         ++activeListIndex;
       }
       ++index;
     }
   }
+
+  private setActiveFirstIndex(val: number) {
+    if (this.isFullScreen) {
+      this.activeListFirstIndexFS = val;
+    } else {
+      this.activeListFirstIndex = val;
+    }
+  }
+
+  private getActiveFirstIndex() {
+    return this.isFullScreen ? this.activeListFirstIndexFS : this.activeListFirstIndex;
+  }
+
+  private setActiveLastIndex(val: number) {
+    if (this.isFullScreen) {
+      this.activeListLastIndexFS = val;
+    } else {
+      this.activeListLastIndex = val;
+    }
+  }
+
+  private getActiveLastIndex() {
+    return this.isFullScreen ? this.activeListLastIndexFS : this.activeListLastIndex;
+  }
+
 
   isPreviousImgExist(): boolean {
     return this.getActive().index > 0;
@@ -55,8 +90,8 @@ export class ImageCarouselComponent implements OnInit {
   previousImg(): void {
     if (!this.isPreviousImgExist()) return;
     if (this.getActive().activeListIndex === 0) {
-      this.activeListFirstIndex -= 1;
-      this.activeListLastIndex -= this.activeListLastIndex > 4 ? 1 : 0;
+      this.setActiveFirstIndex(this.getActiveFirstIndex() - 1);
+      this.setActiveLastIndex(this.getActiveLastIndex() - (this.getActiveLastIndex() > 4 ? 1 : 0));
       this.setActiveList();
       this.setActive(this.getActiveList()[0]);
     } else {
@@ -72,8 +107,8 @@ export class ImageCarouselComponent implements OnInit {
     if (!this.isNextImgExist()) return;
     let activeListMiddleIndex = Math.floor(this.getActiveList().length / 2);
     if (this.getActive().activeListIndex === activeListMiddleIndex) {
-      this.activeListFirstIndex += 1;
-      this.activeListLastIndex += 1;
+      this.setActiveFirstIndex(this.getActiveFirstIndex() + 1);
+      this.setActiveLastIndex(this.getActiveLastIndex() + 1);
       this.setActiveList();
       this.setActive(this.getActiveList()[activeListMiddleIndex]);
     } else {
@@ -86,23 +121,57 @@ export class ImageCarouselComponent implements OnInit {
   }
 
   getActive(): CarouselImg {
+    return this.isFullScreen ? this.getActiveFS() : this.getActiveSS();
+  }
+
+  getActiveFS(): CarouselImg {
+    return this.activeImgFS;
+  }
+
+  getActiveSS(): CarouselImg {
     return this.activeImg;
   }
 
   setActive(img: CarouselImg) {
+    let activeImg: CarouselImg = this.getActive();
     if (img) {
-      if (this.activeImg && this.activeImg.index !== img.index) {
-        this.activeImg.isActive = false;
+      if (activeImg && activeImg.index !== img.index) {
+        activeImg.isActive = false;
       }
       img.isActive = true;
-      this.activeImg = img;
+      activeImg = img;
     } else {
-      this.activeImg = this.getActiveList().length > 0 ? this.getActiveList()[0] : new CarouselImg();
-      this.activeImg.isActive = true;
+      activeImg = this.getActiveList().length > 0 ? this.getActiveList()[0] : new CarouselImg();
+      activeImg.isActive = true;
+    }
+
+    if (this.isFullScreen) {
+      this.activeImgFS = activeImg;
+    } else {
+      this.activeImg = activeImg;
     }
   }
 
   getActiveList() {
-    return this.activeList;
+    return this.isFullScreen ? this.activeListFS : this.activeList;
+  }
+
+  closeFullScreen() {
+    this.isFullScreen = false;
+  }
+
+  openFullScreen() {
+    this.isFullScreen = true;
+    if (this.activeImg) {
+      let firstIndex = this.activeImg.index > 2 ? this.activeImg.index - 2 : this.activeImg.index;
+      let lastIndex = this.activeImg.index < (this.values.length - 3) ? this.activeImg.index + 2 : this.activeImg.index;
+      this.setActiveFirstIndex(firstIndex);
+      this.setActiveLastIndex(lastIndex);
+      this.setActiveList();
+      this.setActive(this.activeImg);
+    } else {
+      this.setActiveList();
+      this.setActive(undefined);
+    }
   }
 }
